0c7ddd7967fba6c61573467819dfed40efc8b95a
itkimura <itkimura@student.hive.fi>
2022-02-07 23:19:39 +0200

    Change error message

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: itkimura <itkimura@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 09:38:41 by itkimura          #+#    #+#             */
-/*   Updated: 2022/02/07 16:39:32 by itkimura         ###   ########.fr       */
+/*   Updated: 2022/02/07 23:19:19 by itkimura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	error(char *str)
 {
-	ft_putendl_fd(str, (int)stderr);
 	ft_putendl_fd(str, 2);
 	return (1);
 }
