9525f3864bc7e013807b84f51f0e88ba53e80e15
Itoe Kimura <itkimura@student.hive.fi>
2022-02-07 19:00:07 +0200

    test validation part

--- print.c
+++ print.c
@@ -6,7 +6,7 @@
 /*   By: itkimura <itkimura@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 12:42:55 by itkimura          #+#    #+#             */
-/*   Updated: 2022/02/07 16:51:55 by itkimura         ###   ########.fr       */
+/*   Updated: 2022/02/07 18:58:37 by itkimura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ int	backtrack(t_tetri *list, int size, uint16_t *map)
 			{
 				*(uint64_t *)(map + y) ^= (list->value >> x);
 				list->pos = x + (y * size);
-				test_print_map(map, size);
 				if (backtrack(list + 1, size, map))
 					return (1);
 				*(uint64_t *)(map + y) ^= (list->value >> x);
